5cafd749573b70766352fa8b0267ff303e110c42
Parami Gunasekara <parami@live.ca>
2017-11-13 20:13:13 -0500

    Imported Firebase libraries

--- app/src/main/java/mgarzon/createbest/productcatalog/MainActivity.java
+++ app/src/main/java/mgarzon/createbest/productcatalog/MainActivity.java
@@ -12,7 +12,15 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
